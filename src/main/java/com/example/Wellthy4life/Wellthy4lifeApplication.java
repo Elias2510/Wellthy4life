1249e6c2c9bb713cd77e022b73f77bfee87516bc
Elias2510 <eliasmilosi2002@gmail.com>
2025-02-13 22:52:06 +0200

    njcdeq

---
+++ src/main/java/com/example/Wellthy4life/Wellthy4lifeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Wellthy4life;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wellthy4lifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wellthy4lifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Wellthy4life/Wellthy4lifeApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.Wellthy4life;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wellthy4lifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
