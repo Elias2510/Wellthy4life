e85faa37efb0dfe3abda00861053535c67e0d31d
Elias2510 <eliasmilosi2002@gmail.com>
2025-02-23 18:44:58 +0200

    add reminders and recomendations

--- src/test/java/com/example/wellthy4life/Wellthy4lifeApplicationTests.java
+++ src/test/java/com/example/wellthy4life/Wellthy4lifeApplicationTests.java
@@ -1,14 +1,15 @@
 package com.example.wellthy4life;
 
 import com.example.wellthy4life.models.Analysis;
+import com.example.wellthy4life.models.Recommendation;
 import com.example.wellthy4life.models.User;
 import com.example.wellthy4life.repositories.AnalysisRepository;
+import com.example.wellthy4life.repositories.RecommendationRepository;
 import com.example.wellthy4life.repositories.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.time.LocalDate;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -22,28 +23,29 @@ public class Wellthy4lifeApplicationTests {
 	@Autowired
 	private AnalysisRepository analysisRepository;
 
+	@Autowired
+	private RecommendationRepository recommendationRepository;
+
 	@Test
-	public void testSimulateAddingAnalysisForUser() {
-		// Simulăm un utilizator conectat
+	public void testSimulateAddingRecommendationForUser() {
+
 		String userEmail = "john.doe@example.com";
 		Optional<User> optionalUser = Optional.ofNullable(userRepository.findByEmail(userEmail));
 		assertThat(optionalUser).isPresent();
 		User user = optionalUser.get();
 
 
-		Analysis analysis1 = new Analysis();
-		analysis1.setUser(user);
-		analysis1.setTestName("Glicemie");
-		analysis1.setValue(98.2);
-		analysis1.setUnit("mg/dL");
-		analysis1.setNormalMin(70.0);
-		analysis1.setNormalMax(110.0);
-		analysis1.setTestDate(LocalDate.now());
-		analysisRepository.save(analysis1);
+		Optional<Analysis> optionalAnalysis = analysisRepository.findByUserId(user.getId()).stream().findFirst();
+		assertThat(optionalAnalysis).isPresent();
+		Analysis analysis = optionalAnalysis.get();
 
+		Recommendation recommendation = new Recommendation();
+		recommendation.setUser(user);
+		recommendation.setAnalysis(analysis);
+		recommendation.setRecommendationText("Este recomandat să verificați nivelul de colesterol lunar.");
+		recommendationRepository.save(recommendation);
 
-
-		assertThat(analysisRepository.findByUserId(user.getId())).hasSizeGreaterThanOrEqualTo(1);
-		System.out.println("Added multiple analyses for user: " + user.getFullName());
+		assertThat(recommendationRepository.findByUserId(user.getId())).isNotEmpty();
+		System.out.println("Recommendation added for user: " + user.getFullName());
 	}
 }
